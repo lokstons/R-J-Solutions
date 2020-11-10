04d47c71ac7107452e6669cd1483763229729085
Lokstons <s.rogeliochacon@gmail.com>
2020-11-09 22:58:48 -0700

    Interbandas

---
+++ R_J_Solutions/app/src/main/java/com/example/r_j_solutions/Info_Area.java
@@ -0,0 +1,14 @@
+package com.example.r_j_solutions;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Info_Area extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_info__area);
+    }
+}
